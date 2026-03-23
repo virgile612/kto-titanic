76c123e5e03fd87ccc30087c167b38c44ed31813
virgile612 <virgile612@outlook.fr>
2026-03-23 13:57:50 +0000

    feat: lancement de la partie MLflow

--- src/titanic/training/main.py
+++ src/titanic/training/main.py
@@ -1,6 +1,7 @@
 import logging
 
 import fire
+import mlflow
 
 from titanic.training.steps.load_data import load_data
 from titanic.training.steps.validate import validate
@@ -9,13 +10,13 @@ from titanic.training.steps.train import train
 
 def workflow(input_data_path: str, n_estimators: int, max_depth: int, random_state: int) -> None:
     logging.warning(f"workflow input path : {input_data_path}")
-    local_path = load_data(input_data_path)
-    xtrain_path, xtest_path, ytrain_path, ytest_path = split_train_test(local_path)
-    model_path = train(xtrain_path, ytrain_path, n_estimators, max_depth, random_state)
-    validate(model_path, xtest_path, ytest_path)
-    # TODO : Dans un second temps, démarrer le run mlflow au début de ce workflow
+    with mlflow.start_run():
+        local_path = load_data(input_data_path)
+        xtrain_path, xtest_path, ytrain_path, ytest_path = split_train_test(local_path)
+        model_path = train(xtrain_path, ytrain_path, n_estimators, max_depth, random_state)
+        validate(model_path, xtest_path, ytest_path)
 
 
 if __name__ == "__main__":
     fire.Fire(workflow)
-    
+
